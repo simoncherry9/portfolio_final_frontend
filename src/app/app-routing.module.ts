2fda9dd377cdb1224d87fd7750338d6e36d1e48f
Simon <116177578+simoncherry9@users.noreply.github.com>
2023-04-29 11:09:23 -0300

    Mejorando Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
+import { LoginComponent } from './componentes/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'portfolio', component:PortfolioComponent },
+  { path: 'login', component:LoginComponent },
+  { path: '', redirectTo: 'portfolio', pathMatch:'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
