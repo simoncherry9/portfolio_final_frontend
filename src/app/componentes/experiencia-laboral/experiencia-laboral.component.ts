7533ea9a8cea6291661b6efba9b090fb4528a402
Simon <116177578+simoncherry9@users.noreply.github.com>
2023-05-04 08:06:32 -0300

    Ya se pueden ver las experiencias laborales de la db

--- src/app/componentes/experiencia-laboral/experiencia-laboral.component.ts
+++ src/app/componentes/experiencia-laboral/experiencia-laboral.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Experiencia } from 'src/app/interfaces/experiencia';
+import { ExperienciasService } from 'src/app/services/experiencia';
 
 @Component({
   selector: 'app-experiencia-laboral',
   templateUrl: './experiencia-laboral.component.html',
   styleUrls: ['./experiencia-laboral.component.css']
 })
-export class ExperienciaLaboralComponent {
+export class ExperienciaLaboralComponent implements OnInit {
+  name: string = '';
+  description: string = '';
+  loading: boolean = false;
+  listExperiencias: Experiencia[] = []
+
+  constructor(private _experienciaLaboralService: ExperienciasService) { }
+
+  ngOnInit(): void {
+    this.getAptitudes();
+  }
+
+  getAptitudes() {
+    this._experienciaLaboralService.getExperiencias().subscribe(data => {
+      this.listExperiencias = data;
+    })
+  }
 
 }
---
+++ src/app/services/experiencia.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Experiencia } from '../interfaces/experiencia';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExperienciasService {
+  private myAppUrl: string;
+  private myApiUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.myAppUrl = 'http://localhost:3001/'
+    this.myApiUrl = 'api/experiencia'
+  }
+
+  Crear(experiencias: Experiencia): Observable<any> {
+    return this.http.post(`${this.myAppUrl}${this.myApiUrl}`, experiencias);
+  }
+
+  getExperiencias(): Observable<Experiencia[]> {
+    return this.http.get<Experiencia[]>(`${this.myAppUrl}${this, this.myApiUrl}`)
+  }
+
+}
