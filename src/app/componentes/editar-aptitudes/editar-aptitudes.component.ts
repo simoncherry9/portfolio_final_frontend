b205c915aeec3c7a6de77a32cc5b589430d4387e
Simon <116177578+simoncherry9@users.noreply.github.com>
2023-05-05 09:13:33 -0300

    Ya se puede editar educacion

--- src/app/componentes/editar-aptitudes/editar-aptitudes.component.ts
+++ src/app/componentes/editar-aptitudes/editar-aptitudes.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Aptitudes } from 'src/app/interfaces/aptitudes';
+import { ErrorService } from 'src/app/services/error.service';
+import { AptitudesService } from 'src/app/services/aptitudes.service';
+
 
 @Component({
   selector: 'app-editar-aptitudes',
   templateUrl: './editar-aptitudes.component.html',
   styleUrls: ['./editar-aptitudes.component.css']
 })
-export class EditarAptitudesComponent {
+export class EditarAptitudesComponent implements OnInit {
+  name: string = '';
+  description: string = '';
+  loading: boolean = false;
+  listAptitudes: Aptitudes[] = []
 
+  constructor(private toastr: ToastrService,
+    private _aptitudesService: AptitudesService,
+    private router: Router,
+    private _errorService: ErrorService) { }
+
+  ngOnInit(): void {
+    this.getAptitudes();
+  }
+
+  getAptitudes() {
+    this._aptitudesService.getAptitudes().subscribe(data => {
+      this.listAptitudes = data;
+    })
+  }
+
+  addAptitud() {
+
+    // Validamos que el usuario ingrese valores
+    if (this.name == '' || this.description == '') {
+      this.toastr.error("Todos los campos son obligatorios", "Error");
+      return;
+    }
+
+    // Creamos el objeto 
+    const aptitudes: Aptitudes = {
+      name: this.name,
+      description: this.description
+    }
+
+    this.loading = true;
+
+    this._aptitudesService.Crear(aptitudes).subscribe({
+      next: (v) => {
+        this.loading = false;
+        this.toastr.success("La aptitud " + this.name + " fue cargada con exito", "Aptitud agregada");
+        this.router.navigate(['/editar']);
+        location.reload()
+      },
+      error: (e: HttpErrorResponse) => {
+        this.loading = false;
+        this._errorService.msjError(e);
+      }
+    })
+
+  }
 }
